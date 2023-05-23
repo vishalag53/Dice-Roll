005a85a9a6d351ddb25d97e1b742b45ca51a14d9
vishalag53 <vishal.agrawal1207@gmail.com>
2023-05-23 10:01:03 +0530

    Add

---
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.diceroll
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import com.example.diceroll.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var diceImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val rollButton: Button = binding.rollButton
+
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+
+        diceImage  = binding.diceImage
+    }
+
+    private fun rollDice(){
+        val randNum = Random.nextInt(6) + 1
+
+        val draRes = when(randNum){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(draRes)
+
+    }
+}
